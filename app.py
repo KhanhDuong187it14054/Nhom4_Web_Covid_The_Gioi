b95971f100ba2675117b8201b9015c65276dfeef
KhanhDuong187it14054 <duong.187it14054@vanlanguni.vn>
2021-11-02 22:57:32 +0700

    Update master

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 from flask import session
 
-ungdung = Flask(__name__, static_folder="./static")
+app = Flask(__name__, static_folder="./static")
 
-@ungdung.route('/')
-@ungdung.route('/index.html')
+@app.route('/')
+@app.route('/index.html')
 def index():
   return render_template("index.html")
 
-@ungdung.route('/tin_tuc')
-@ungdung.route('/tin_tuc.html')
+@app.route('/tin_tuc')
+@app.route('/tin_tuc.html')
 def tin_tuc():
     return render_template("tin_tuc.html")
 
-@ungdung.route('/tiem_chung')
-@ungdung.route('/tiem_chung.html')
+@app.route('/tiem_chung')
+@app.route('/tiem_chung.html')
 def tiem_chung():
   return render_template("tiem_chung.html")
 
 if __name__ == "__main__":
-  ungdung.run(port = 5050)
+  app.run(port = 5050)
